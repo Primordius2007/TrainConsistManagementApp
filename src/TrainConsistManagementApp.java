1d1c23e70d484c76263d8ffda354e08799582c17
Primordius2007 <narayanparasher007@gmail.com>
2026-04-01 15:59:22 +0530

    UC4: MAINTAIN ORDERED

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
@@ -67,5 +68,34 @@ public class TrainConsistManagementApp {
         System.out.println("Duplicates are automatically ignored by HashSet.");
 
         System.out.println("\nUC3 uniqueness validation completed...");
+
+        // UC4 - Maintain Ordered Bogie Consist
+        System.out.println("\n========================================");
+        System.out.println(" UC4 - Maintain Ordered Bogie Consist ");
+        System.out.println("========================================\n");
+
+        LinkedList<String> consist = new LinkedList<>();
+
+        consist.add("Engine");
+        consist.add("Sleeper");
+        consist.add("AC");
+        consist.add("Cargo");
+        consist.add("Guard");
+
+        System.out.println("Initial Train Consist:");
+        System.out.println(consist);
+
+        consist.add(2, "Pantry Car");
+
+        System.out.println("\nAfter Inserting 'Pantry Car' at position 2:");
+        System.out.println(consist);
+
+        consist.removeFirst();
+        consist.removeLast();
+
+        System.out.println("\nAfter Removing First and Last Bogie:");
+        System.out.println(consist);
+
+        System.out.println("\nUC4 ordered consist operations completed...");
     }
 }
